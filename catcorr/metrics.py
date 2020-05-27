8c62804f4eda1c638bdb8cf1ed849cf478c5d052
Jerod Weinman <jerod@acm.org>
2020-05-27 16:27:15 -0500

    Added TFv1-style metric rk_coeff

--- catcorr/metrics.py
+++ catcorr/metrics.py
@@ -1,9 +1,93 @@
-# RkMetric.py - Class to track Rk value using the Estimator Metric functionality
+# metrics.py - Folderoll (classes, tensors, ops) for metrics supporting Rk
 
 import tensorflow as tf
 import tensorflow.keras
 
-import catcorr
+from catcorr import catcorr
+
+""" For background details, refer to http://rk.kvl.dk and
+
+      Gorodkin, J. (2004) Comparing two K-category assignments by a 
+         K-category correlation coefficient. J. Comp. Biol. and Chem.,
+         28:367--374. https://doi.org/10.1016/j.compbiolchem.2004.09.006
+"""
+
+# rk_coeff documentation modeled after tf.compat.v1.metrics.accuracy
+# <https://www.tensorflow.org/api_docs/python/tf/compat/v1/metrics/accuracy>
+# Used under the terms of a Creative Commons Attribution 4.0 License
+# <https://creativecommons.org/licenses/by/4.0/>
+
+def rk_coeff(labels, predictions, num_classes,
+             streaming=True,
+             name=None):
+    """Calculates Gorodkin's Rk correlation coefficient.
+
+    The `rk_coeff` function creates a local variable, `table` to store
+    the confusion matrix used to compute the Rk coefficient. That value
+    is ultimately returned as `coeff`: an idempotent operation.
+
+    For estimation of the metric over a stream of data (indicated by
+    `streaming`), the function creates an `update_op` operation that
+    updates these variables and returns the `coeff`. The `update_op`
+    accumulates values in the confusion matrix.
+
+    Let N be batch size and L be the number of class labels.
+
+    Parameters:
+      labels      : 1-D (rank 1) tensor (length N) of ground-truth labels
+                      (indices) in {0,...,L-1} or 2-D (rank 2) NxL tensor
+                      of one-hot encoded labels 
+      predictions : 2-D (rank 2) tensor (size NxL) probabilities for each
+                      class in [0,1]
+      num_classes : An scalar integer value indicating the number of class
+                      labels (length of one side of the confusion matrix
+                      table)
+    Returns:
+      coefficient : A `Tensor` representing the Rk coefficient of the `table`.
+      update_op :   An operation that increments the `table` variable
+                      appropriately and whose value matches `coefficient`.
+
+    """
+
+    #var_collections = [ tf.compat.v1.GraphKeys.LOCAL_VARIABLES,
+    #                    tf.compat.v1.GraphKeys.METRIC_VARIABLES ]
+    
+    batch_table = catcorr.soft_confusion_matrix_tf(
+        labels, predictions, name='rk_coeff/batch_table' )
+
+    # After futzing for hours, I could not get a more elegant solution
+    # inferring the dimensions of batch_table to work with v1-oriented
+    # TF code, (it is a variale initialization issue) which
+    # necessitates the num_classes argument to hard code the
+    # dimensions
+
+    # This line in and of itself works, but fails when passed to tf.Variable
+    #zero_table = tf.zeros_like(batch_table)
+    # So we fall back to the following:
+    zero_table = tf.zeros([num_classes,num_classes],dtype=batch_table.dtype)
+
+    #table = tf.compat.v1.Variable( zero_table, dtype=batch_table.dtype),
+    #                               trainable=False,
+    #                               name='rk_coeff/table',
+    #                               collections=var_collections,
+    #                               aggregation=tf.VariableAggregation.SUM )
+    table = tf.Variable( zero_table,
+                         trainable=False,
+                         validate_shape=True,
+                         name='rk_coeff/table',
+                         dtype=batch_table.dtype,
+                         aggregation=tf.VariableAggregation.SUM )
+
+    if streaming: # Accumulate confusion matrix
+        update_table_op = tf.compat.v1.assign_add(table, batch_table) # +=
+    else: # Reset confusion matrix
+        update_table_op = tf.compat.v1.assign(table, batch_table)     # := 
+
+    coeff = tf.identity( catcorr.rk_coeff_tf(table), name='rk_coeff/value')
+    update_op = tf.identity( catcorr.rk_coeff_tf(update_table_op),
+                             name='rk_coeff/update_op')
+    return coeff, update_op
+
 
 class RkMetric(tf.keras.metrics.Metric):
     """
