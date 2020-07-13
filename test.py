b0aa337b80bc50a2a4cb67c19e0fac1e402f0b4e
Nathan Gifford <gifforde@grinnell.edu>
2020-07-13 15:23:33 -0500

    Switches to eager execution on test cases for compatibility wiht TF2.x

--- test.py
+++ test.py
@@ -68,35 +68,30 @@ class TestRkCoeffTensorFlow(unittest.TestCase):
     
     def test_zero(self):
         zeros = tf.constant(np.zeros((4, 4)))
-        result_tensor = catcorr.rk_coeff_tf(zeros)
-        result = self.sess.run(result_tensor)
+        result = catcorr.rk_coeff_tf(zeros)
         self.assertEqual(result, 0)
 
     def test_ones(self):
         ones = tf.constant(np.ones((4, 4)))
-        result_tensor = catcorr.rk_coeff_tf(ones)
-        result = self.sess.run(result_tensor)
+        result = catcorr.rk_coeff_tf(ones)
         self.assertEqual(result, 0)
 
     def test_arange(self):
         precalculated_result = -0.07007127538
         arange = tf.constant(np.arange(9, dtype=np.float).reshape((3,3)))
-        result_tensor = catcorr.rk_coeff_tf(arange)
-        result = self.sess.run(result_tensor)
+        result = catcorr.rk_coeff_tf(arange)
         difference = abs(precalculated_result - result)
         self.assertLess(difference, 0.00000001)
 
     def test_large_table(self):
         large_table = tf.constant(np.ones((500, 500)))
-        result_tensor = catcorr.rk_coeff_tf(large_table)
-        result = self.sess.run(result_tensor)
+        result = catcorr.rk_coeff_tf(large_table)
         self.assertEqual(result, 0)
 
     def test_arange_2(self):
         precalculated_result = -0.031676277618
         arange = tf.constant(np.arange(16, dtype=np.float).reshape((4,4)))
-        result_tensor = catcorr.rk_coeff_tf(arange)
-        result = self.sess.run(result_tensor)
+        result = catcorr.rk_coeff_tf(arange)
         difference = abs(precalculated_result - result)
         self.assertLess(difference, 0.00000001)
 
@@ -108,16 +103,14 @@ class TestRkCoeffTensorFlow(unittest.TestCase):
         precalculated_result = -0.333
         confusion_matrix = np.full((4,4), 5)
         np.fill_diagonal(confusion_matrix, 0)
-        result_tensor = catcorr.rk_coeff_tf(tf.constant(confusion_matrix))
-        result = self.sess.run(result_tensor)
+        result = catcorr.rk_coeff_tf(tf.constant(confusion_matrix))
         difference = abs(precalculated_result - result)
         self.assertLess(difference, 0.001)
 
     def test_col(self):
         confusion_matrix = np.zeros((4,4))
         confusion_matrix[:,1] = [15,15,15,15]
-        result_tensor = catcorr.rk_coeff_tf(tf.constant(confusion_matrix))
-        result = self.sess.run(result_tensor)
+        result = catcorr.rk_coeff_tf(tf.constant(confusion_matrix))
         self.assertEqual(result, 0)
         
     def tearDown(self):
@@ -132,8 +125,7 @@ class TestSoftConfusionMatrixTensorFlow(unittest.TestCase):
     def test_identity_labels_vector(self):
         labels = tf.constant( np.arange(10) )
         probs = tf.constant( np.eye(10) )
-        result_tensor = catcorr.soft_confusion_matrix_tf(labels, probs)
-        result = self.sess.run(result_tensor)
+        result = catcorr.soft_confusion_matrix_tf(labels, probs)
         expected = np.eye(10)
         
         self.assertTrue( np.array_equal( result, expected ) )
@@ -141,8 +133,7 @@ class TestSoftConfusionMatrixTensorFlow(unittest.TestCase):
     def test_identity_labels_matrix(self):
         labels = tf.constant( np.eye(10,dtype=np.int32) )
         probs = tf.constant( np.eye(10) )
-        result_tensor = catcorr.soft_confusion_matrix_tf(labels, probs)
-        result = self.sess.run(result_tensor)
+        result = catcorr.soft_confusion_matrix_tf(labels, probs)
         expected = np.eye(10)
 
         self.assertTrue( np.array_equal( result, expected ) )
